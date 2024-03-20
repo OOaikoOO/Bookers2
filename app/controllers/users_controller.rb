6c5033955ce4910774f40178f8bcd7ca49514202
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-20 14:51:28 +0000

    userinfo,newbook完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
+  end
+
+  def index
+    @users = User.all
   end
 end
