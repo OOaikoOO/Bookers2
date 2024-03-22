8cfd6610b7d38e2282d39c700b5ae8286456abb4
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-22 16:48:26 +0000

    flashmessage完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_up_path_for(resource)
+    flash[:signup_notice] = "Welcome! You have signed up successfully."
     user_path(current_user)
   end
+
+  def after_sign_in_path_for(resource)
+    flash[:signin_notice] = "Signed in successfully."
+    user_path(current_user)
+  end
+
   
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
     root_path
   end
 
@@ -14,5 +22,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+  
+  def set_flash_message(kind, message)
+    flash[kind] = message
   end
 end
