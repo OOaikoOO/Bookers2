b6a149bbc686d3fb06e4998512e450cdd6e3d7d6
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-22 13:14:50 +0000

    users_index完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -12,34 +12,34 @@ class BooksController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
     render 'show'
   end
-  
+
   def index
     @books = Book.all
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body).merge(user_id: current_user.id)
   end
