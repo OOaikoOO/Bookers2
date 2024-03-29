e65f90991e0394e77af4468950cc3410f1798961
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-29 15:12:54 +0000

    [Add]いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
+  
   def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_to books_path
   end
 
   def destroy
+    favorite = current_user.favorites.find_by(book_id: params[:book_id])
+    favorite.destroy if favorite
+    redirect_to books_path
   end
 end
