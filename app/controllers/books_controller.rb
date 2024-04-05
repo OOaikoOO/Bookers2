a3c62a9bff70901819e5218272b2be0639d9a2ac
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-04-05 13:48:42 +0000

    [Add]いいね機能過去一週間合計カウント多い順に表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def new
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
-    @user = current_user 
+    @user = current_user
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
@@ -25,8 +25,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort_by { |book|
+        -book.favorites.where(created_at: from...to).count
+      }
     @book = Book.new
-    @books = Book.all
     @user = current_user
   end
 
@@ -50,13 +55,13 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body).merge(user_id: current_user.id)
   end
-  
+
   def is_matching_login_user
     begin
       book = Book.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
+  
   has_many :book_comments, dependent: :destroy
+  
   has_many :favorites, dependent: :destroy
   
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   
